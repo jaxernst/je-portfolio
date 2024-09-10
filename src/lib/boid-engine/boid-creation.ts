9f5ed9222b44e73087a884e65f07ab9ebf3236bd
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-09-10 14:49:25 -0700

    Creation distribution tuning

--- src/lib/boid-engine/boid-creation.ts
+++ src/lib/boid-engine/boid-creation.ts
@@ -14,22 +14,22 @@ type AttributeProbabilityDistribution<T> = {
 };
 
 const attributeRanges: AttributeProbabilityDistribution<Partial<BoidAttrs>> = {
-  mass: { min: 0.05, max: 0.3, mean: 0.15, stdev: 0.1, skew: 0 },
-  targetV: { min: 5, max: 200, mean: 100, stdev: 50, skew: 0 },
+  mass: { min: 0.05, max: 0.2, mean: 0.1, stdev: 0.05, skew: 0 },
+  targetV: { min: 5, max: 200, mean: 50, stdev: 20, skew: 0 },
   targetVCorrectionFactor: {
-    min: 0.01,
-    max: 5,
-    mean: 2,
-    stdev: 2,
+    min: 0.06,
+    max: 3,
+    mean: 0.8,
+    stdev: 0.3,
     skew: 0,
   },
-  sightRadius: { min: 50, max: 400, mean: 200, stdev: 20, skew: 0 },
-  sightPeripheralDeg: { min: 130, max: 360, mean: 200, stdev: 70, skew: 0 },
-  separationDistance: { min: 5, max: 100, mean: 30, stdev: 20, skew: 0 },
-  separationFactor: { min: 0.5, max: 3, mean: 2, stdev: 0.5, skew: 0 },
+  sightRadius: { min: 50, max: 600, mean: 300, stdev: 100, skew: 0 },
+  sightPeripheralDeg: { min: 130, max: 300, mean: 180, stdev: 30, skew: 0 },
+  separationDistance: { min: 5, max: 200, mean: 100, stdev: 50, skew: 0 },
+  separationFactor: { min: 0.5, max: 3, mean: 1, stdev: 0.2, skew: 0 },
   gravitationFactor: { min: 0.1, max: 2, mean: 0.8, stdev: 0.5, skew: 0 },
   alignmentFactor: { min: 0.05, max: 0.5, mean: 0.1, stdev: 0.4, skew: 0 },
-  forceSmoothing: { min: 0, max: 20, mean: 5, stdev: 5, skew: 0 },
+  forceSmoothing: { min: 0, max: 20, mean: 0, stdev: 0.05, skew: 0 },
 };
 
 export const randomizeBoidType = (deviationFactor: number = 1) => {
